cb86b9c97fa5c59f409c425c9a068389d6097fda
intae92 <kangit2013@gmail.com>
2021-01-08 21:23:09 +0900

    feat: DOM 요소 id 값 상수화

---
+++ src/utils/variables.js
@@ -0,0 +1,9 @@
+export const DOM_ID = {
+  DEPARTURE_STATION_NAME_INPUT_ID: "departure-station-name-input",
+  ARRIVAL_STATION_NAME_INPUT_ID: "arrival-station-name-input",
+  SEARCH_BUTTON_ID: "search-button",
+  SHORTEST_DISTANCE_RADIO_ID: "shortest-distance-radio",
+  MINIMUM_DISTANCE_RADIO_ID: "minimum-distance-radio",
+  SEARCH_BUTTON_ID: "search-button",
+  SEARCH_TYPE_NAME: "search-type",
+};
