801ce5905370b6571cd52d18875239dbfcd61634
intae92 <kangit2013@gmail.com>
2021-01-08 19:42:06 +0900

    feat: View 설정

---
+++ src/views/View.js
@@ -0,0 +1,22 @@
+export default class View {
+  constructor() {
+    this.tag = "[View]";
+  }
+
+  init(el) {
+    if (!el) throw el;
+    this.el = el;
+    return this;
+  }
+
+  on(event, handler) {
+    this.el.addEventListener(event, handler);
+    return this;
+  }
+
+  emit(event, data) {
+    const evt = new CustomEvent(event, { detail: data });
+    this.el.dispatchEvent(evt);
+    return this;
+  }
+}
